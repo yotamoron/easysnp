6bcb692f95b8377956349e074a1cb1c40075990f
Yotam Oron <yotamoron@yahoo.com>
2012-01-07 14:17:49 +0200

    Further template enhancements

--- easysnp/main/views.py
+++ easysnp/main/views.py
@@ -4,10 +4,9 @@ import main.ajax as ajax
 from django.shortcuts import render_to_response
 
 def index(request):
-    return render_to_response('index.html',
-        {
-            'my_leads': ajax.load_url('/leads/my_leads.html'),
-            'my_tasks': ajax.load_url('/tasks/my_tasks.html')
-         })
+    panels = [ ajax.load_url('/leads/my_leads.html'),
+               ajax.load_url('/tasks/my_tasks.html') ]
+    return render_to_response('base.html',
+            {'body': panels, 'title': 'Hello Kitty' })
 
 
