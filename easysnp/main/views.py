c8fcd7c4f48a45984954d736845765d4f4e45b33
Yotam Oron <yotamoron@yahoo.com>
2012-01-10 23:29:48 +0200

    - Login now works - Added logout template - Added an example for login_required usage

--- easysnp/main/views.py
+++ easysnp/main/views.py
@@ -1,14 +1,26 @@
 # Create your views here.
 
+from django.contrib.auth.decorators import login_required
 import main.ajax as ajax
 from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from urlparse import urlparse
 
 def index(request):
+    #if request.META.has_key('HTTP_REFERER'):
+    #    referer = urlparse(request.META['HTTP_REFERER'])
+    #    if referer.path == '/account/login/':
+    #        query_args_list = referer.query.split('?')
+    #        next_path = [arg for arg in query_args_list if arg.startswith('next')]
+    #        if next_path:
+    #            return HttpResponseRedirect(next_path[0].split('=')[1])
+
     panels = [ ajax.call('/leads/my_leads.html'),
                ajax.call('/tasks/my_tasks.html') ]
     return render_to_response('base.html',
             {'body': panels, 'title': 'Hello Kitty' })
 
+@login_required
 def leads(request):
     panels = [ ajax.call('/leads/leads.html') ]
     return render_to_response('base.html',
